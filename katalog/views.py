c8bd272c4dd718020d7d6846594c146708a28b76
ArdhitoNurhadyansah <ardhitonurhadyansah66@gmail.com>
2022-09-13 11:42:28 +0700

    Improve functionalities of program based on Model-Views-Template Concept

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    catalog_items_data = CatalogItem.objects.all()
+    context = {
+        'catalog_list' : catalog_items_data,
+        'name' : 'Ardhito Nurhadyansah',
+        'NPM'  : "2106750206",
+    }
+    return render(request, "katalog.html", context)
